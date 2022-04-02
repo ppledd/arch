e147e7077f481e865e8955ff0ca3adb6b833daae
zhengjy <zjy@33.cn>
2022-04-02 16:47:31 +0800

    修改扩展方法

--- arch/src/main/java/com/zjy/architecture/ext/ImageExt.kt
+++ arch/src/main/java/com/zjy/architecture/ext/ImageExt.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 
 /**
@@ -12,7 +13,8 @@ import com.bumptech.glide.request.RequestOptions
  * @since 2020/05/18
  * Description:ImageView扩展函数
  */
-fun ImageView.load(
+@Deprecated("使用load")
+fun ImageView.load0(
     url: String?,
     placeholder: Int = 0,
     block: RequestBuilder<Drawable>.(Int) -> RequestBuilder<Drawable> = DEFAULT_APPLY
@@ -22,7 +24,8 @@ fun ImageView.load(
     }.into(this)
 }
 
-fun ImageView.load(
+@Deprecated("使用load")
+fun ImageView.load0(
     uri: Uri?,
     placeholder: Int = 0,
     block: RequestBuilder<Drawable>.(Int) -> RequestBuilder<Drawable> = DEFAULT_APPLY
@@ -38,4 +41,26 @@ private val DEFAULT_APPLY: RequestBuilder<Drawable>.(Int) -> RequestBuilder<Draw
     } else {
         apply(RequestOptions().placeholder(it).error(it))
     }
+}
+
+fun ImageView.load(
+    uri: String?,
+    placeholder: Int = 0,
+    crossFade: Boolean = false,
+    block: RequestOptions.() -> RequestOptions
+) {
+    Glide.with(this).load(uri).let {
+        if (crossFade) it.transition(DrawableTransitionOptions.withCrossFade()) else it
+    }.apply(RequestOptions().placeholder(placeholder).block()).into(this)
+}
+
+fun ImageView.load(
+    uri: Uri?,
+    placeholder: Int = 0,
+    crossFade: Boolean = false,
+    block: RequestOptions.() -> RequestOptions
+) {
+    Glide.with(this).load(uri).let {
+        if (crossFade) it.transition(DrawableTransitionOptions.withCrossFade()) else it
+    }.apply(RequestOptions().placeholder(placeholder).block()).into(this)
 }
