ba9478a176707beac2b0e90a5cf377f2f529d308
zhengjy <zjy@33.cn>
2022-04-21 15:02:00 +0800

    修改尺寸适配

--- arch/src/main/java/com/zjy/architecture/ext/ScreenExt.kt
+++ arch/src/main/java/com/zjy/architecture/ext/ScreenExt.kt
@@ -52,8 +52,9 @@ val Context.screenSize: Point
  * 适配统一分辨率，宽度统一为360dp
  */
 fun setCustomDensity(activity: Activity, dp: Int) {
+    if (dp == 0) return
     val applicationMetrics = Resources.getSystem().displayMetrics
-    val targetDensity: Float = (applicationMetrics.widthPixels / dp).toFloat()
+    val targetDensity: Float = applicationMetrics.widthPixels.toFloat() / dp
     val targetDensityDpi = 160 * targetDensity
 
     applicationMetrics.density = targetDensity
